28b3596dc9ec30b79449379b52a763470cea9da1
Alex Grinman <grinman.alex@gmail.com>
2016-04-24 16:02:43 -0400

    updated client id/secret

--- util.go
+++ util.go
@@ -2,7 +2,7 @@ package main
 
 //MARK: Constants
 const (
-	GoogleDriveClientSecret = string(`{"installed":{"client_id":"860746801632-2lklu0e3jjv2lqfte5vo2kvlu5cod3g5.apps.googleusercontent.com","project_id":"premium-node-128517","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"w9zqAypudT5F2qLA-w6E3oM3","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}`)
+	GoogleDriveClientSecret = string(`{"installed":{"client_id":"713278088797-agohh4u0l5vjscrmn7j0b79i54mtlein.apps.googleusercontent.com","project_id":"peerless-tiger-129119","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"wd3-G0rEx4VwuV9gwabi1kbj","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}`)
 	DropboxClientKey        = "zpy424sdnluk9c1"
 	DropboxClientSecret     = "rrmjsz7mlgnholq"
 )
